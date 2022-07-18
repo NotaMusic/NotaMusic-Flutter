a1a541cdc5590c74fb9a0dfc37ae69f1ff9c9cf6
Andrey Kiryantsev <reyzor2142@gmail.com>
2022-07-18 21:54:59 +0400

    upd

--- lib/screens/main_page/motor_tab/motor_tab_cubit.dart
+++ lib/screens/main_page/motor_tab/motor_tab_cubit.dart
@@ -1,13 +1,12 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:nota_music/blocs/auth/auth_cubit.dart';
 import 'package:nota_music/screens/main_page/motor_tab/motor_tab_state.dart';
 import 'package:yandex_music_api_flutter/yandex_music_api_flutter.dart';
 
-
-
 //TODO На счет Моей волны.
-// Кажись, что это просто радио с ID user:onyourwave. Но в приложении (Android) 
+// Кажись, что это просто радио с ID user:onyourwave. Но в приложении (Android)
 //используются другие эндпоинты. Но старые, те что в библиотеке тоже работают
 
 class MotorTabCubit extends Cubit<MotorTabState> {
@@ -19,15 +18,18 @@ class MotorTabCubit extends Cubit<MotorTabState> {
     emit(const MotorTabState.loading());
     try {
       final resp = await Client.instance.getRotorStationList();
-      final stations = resp?.map((e) => e.station).where((element) => element != null).cast<Station>().toList();
+      final stations =
+          resp?.map((e) => e.station).where((element) => element != null).cast<Station>().toList();
       if (stations != null) {
         emit(MotorTabState.loaded(stations: stations));
       } else {
         emit(const MotorTabState.error(error: 'Яндекс не вернул список станций'));
       }
     } catch (ex, stack) {
-      print(ex);
-      print(stack);
+      if (kDebugMode) {
+        print(ex);
+        print(stack);
+      }
 
       emit(MotorTabState.error(error: 'Ошибка: ${ex.toString()}'));
     }
